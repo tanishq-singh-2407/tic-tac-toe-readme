ede814a87374334a8b2e6410bda73e8e8be54d5c
Tanishq Singh <tanishq-singh-2301@users.noreply.github.com>
2022-09-30 14:34:24 +0530

    CI: Updated data.json and README.md

---
+++ .husky/repo-update.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+git config --global user.name "Tanishq Singh"
+git config --global user.email "tanishq-singh-2301@users.noreply.github.com"
+git add .
+
+clean=$(git commit -m "CI: Updated data.json and README.md")
+
+if [[ ${clean} == *"nothing to commit"* ]]; then
+    # exit 0
+    echo "asdf"
+else
+    # git push
+    echo "kjh"
+fi
