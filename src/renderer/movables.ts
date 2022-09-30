426f8968484b169b81f392e09d16ae6dd9a1a14b
Tanishq Singh <tanishqsingh640@gmail.com>
2022-09-30 13:50:04 +0530

    Readme renderer updated

--- src/renderer/movables.ts
+++ src/renderer/movables.ts
@@ -3,18 +3,18 @@ import type { DataJson, Moves } from "../../types/index.ts";
 const _1 = (isCompleted: boolean) => `\n\n### **${isCompleted ? "Start a new game..." : "It's your turn..."} make a _move_...**` + "\n\n";
 const _2 = () => "|FROM|TO - _just click one of the links_ :)|" + "\n";
 const _3 = () => "|-|-|" + "\n";
-const __4 = (no: number, move: Moves) => `|**${no}**|[${move}](https://github.com/tanishq-singh-2301/tic-tac-toe-readme/issues/new?title=ttt%7move%7${move}&body=Just+push+%27Submit+new+issue%27.+You+don%27t+need+to+do+anything+else.)|` + "\n";
+const __4 = (no: number, move: Moves, slf: string) => `|**${no}**|<a target="_blank" rel="noopener" href="https://github.com/tanishq-singh-2301/tic-tac-toe-readme/issues/new?title=ttt_move_${move.toLowerCase()}_${slf}_&labels=make+move&body=Jush+push+'Submit+new+issue'.+You+don't+need+to+do+anything+else.">${move}</a>|` + "\n";
 
 export const renderMovables = (data: DataJson) => {
     let rendered = _1(data.mode === "finished") + _2() + _3();
 
     if (data.whoWon.user === "")
-        data.movableMoves.map((val, index) => rendered += __4(index + 1, val));
+        data.movableMoves.map((val, index) => rendered += __4(index + 1, val, data.slf));
     
     else
         ["A", "B", "C"].map((parent, p_ind) =>
             ["1", "2", "3"].map((box, b_ind) =>
-                rendered += __4(((p_ind * 3) + b_ind) + 1, (parent + box) as Moves))
+                rendered += __4(((p_ind * 3) + b_ind) + 1, (parent + box) as Moves, ""))
         );
     
     return rendered;
