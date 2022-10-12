874f2487681b955be187fc496fc696a1e560cd1c
Edwin, A.J <100132896+niwdE-JA@users.noreply.github.com>
2022-10-12 09:49:46 +0100

    Fixed typo in source, rather than generated readme

--- src/renderer/movables.ts
+++ src/renderer/movables.ts
@@ -3,7 +3,7 @@ import type { DataJson, Moves } from "../../types/index.ts";
 const _1 = (isCompleted: boolean) => `\n\n### **${isCompleted ? "Start a new game..." : "It's your turn..."} make a _move_...**` + "\n\n";
 const _2 = () => "|FROM|TO - _just click one of the links_ :)|" + "\n";
 const _3 = () => "|-|-|" + "\n";
-const __4 = (no: number, move: Moves, slf: string) => `|**${no}**|<a target="_blank" rel="noopener" href="https://github.com/tanishq-singh-2301/tic-tac-toe-readme/issues/new?title=_ttt_move_${move.toLowerCase()}_${slf}_&labels=make+move&body=Jush+push+'Submit+new+issue'.+You+don't+need+to+do+anything+else.">${move}</a>|` + "\n";
+const __4 = (no: number, move: Moves, slf: string) => `|**${no}**|<a target="_blank" rel="noopener" href="https://github.com/tanishq-singh-2301/tic-tac-toe-readme/issues/new?title=_ttt_move_${move.toLowerCase()}_${slf}_&labels=make+move&body=Just+push+'Submit+new+issue'.+You+don't+need+to+do+anything+else.">${move}</a>|` + "\n";
 
 export const renderMovables = (data: DataJson) => {
     let rendered = _1(data.mode === "finished") + _2() + _3();
